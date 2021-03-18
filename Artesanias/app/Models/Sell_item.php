ed941d142cb5f1c1e6a3cd7ea1e71a3d59926314
erikapbg98 <73491156+erikapbg98@users.noreply.github.com>
2021-03-17 18:46:59 -0700

    Modelos

---
+++ Artesanias/app/Models/Sell_item.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sell_item extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id_sell',
+        'id_product',
+        'price',
+        'quantity'
+    ];
+}
