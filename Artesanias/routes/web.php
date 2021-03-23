7a80d3c0f86a5fd3b3fc7aa9b9b2fc7aef5b5351
erikapbg98 <73491156+erikapbg98@users.noreply.github.com>
2021-03-22 22:40:50 -0700

    rutas

--- Artesanias/routes/web.php
+++ Artesanias/routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return view('hola');});
+
+Route::get('/producto/{id}/{nombre}', function ($id,$nombre) {
+    return view('verproducto')->with('id', $id)->with('nombre',$nombre);
+});
+
+Route::get('/contacto', function () {
+    $contacto="Erika Barrera";
+    $valores=10;
+    $color="#CCC";
+    return view('contacto')
+    ->with('nombre',$contacto)
+    ->with('fondo', $color)
+    ->with('valores',$valores);
+    
 });
