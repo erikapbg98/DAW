ee2db779e33fa6758cf053a59fcd924354a27d10
erikapbg98 <73491156+erikapbg98@users.noreply.github.com>
2021-03-28 16:46:47 -0700

    practica

---
+++ Artesanias/resources/views/practica.blade.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/styles.css')}}">
+    
+</head>
+<body>
+   
+      <div class="container">
+        <div class="row">
+        <div class="col-md-12 mt-5  d-lg-block d-none d-xl-block"><div class="p-3 border bg-primary text-center heigth_a" >A</div></div>
+            <div class="col-md-12 mt-5  d-lg-none d-xl-none d-m-block"><div class="p-3 border bg-primary text-center heigth_a">L</div></div>
+        </div>
+  
+        <div class="row">
+        <div class="col mt-3 d-none d-sm-block"><div class="p-3 border bg-primary text-center heigth_b" >B</div></div>
+        <div class="col mt-3  "><div class="p-3 border bg-primary text-center heigth_b" >C</div></div>
+        <div class="col mt-3  "><div class="p-3 border bg-primary text-center heigth_b" >D</div></div>
+        <div class="col mt-3  d-lg-block d-xl-block d-m-block d-none"><div class="p-3 border bg-primary text-center heigth_b" >E</div></div>
+        </div>
+        <div class="row">
+        <div class="col mt-3  "><div class="p-3 border bg-primary text-center heigth_b" >F</div></div>  
+        <div class="col mt-3 d-none d-sm-block"><div class="p-3 border bg-primary text-center heigth_b" >G</div></div>
+        </div>
+        <div class="row">
+        <div class="col-9 mt-3 d-lg-block d-none d-xl-block"><div class="p-3 border bg-primary text-center heigth_d" >H</div></div>
+        <div class="col-3 mt-3 d-lg-block d-none d-xl-block"><div class="p-3 border bg-primary text-center heigth_d" >I</div></div>
+        <div class="col mt-3 d-block d-sm-none"><div class="p-3 border bg-primary text-center heigth_b" >M</div></div>
+            <div class="col mt-3 d-none d-sm-block d-lg-none d-xl-none d-m-block"><div class="p-3 border bg-primary text-center heigth_d">H</div></div>
+        </div>
+        <div class="row">
+
+        <div class="col mt-3 d-block d-sm-none"><div class="p-3 border bg-primary text-center heigth_d" >H</div></div>
+        <div class="col mt-3 d-none d-md-block d-lg-none d-none d-lg-block d-xl-none"><div class="p-3 border bg-primary text-center heigth_d" >I</div></div>
+        </div>
+        <div class="row">
+        <div class="col-1"></div>
+        <div class="col-4 mt-3 m d-none d-xl-block"><div class="p-3 border bg-primary text-center heigth_d" >J</div></div>
+        <div class="col-2 mt-3 m d-none d-xl-block"><div></div></div>
+        <div class="col-4 mt-3 d-none d-xl-block"><div class="p-3 border bg-primary text-center heigth_d" >K</div></div>
+        <div class="col-1"></div>
+        </div>
+       
+        <div class="row">
+
+<div class="col mt-3 d-block d-sm-none"><div class="p-3 border bg-primary text-center heigth_d" >I</div></div>
+<div class="col mt-3 d-none d-md-block d-lg-none d-none d-lg-block d-xl-none"><div class="p-3 border bg-primary text-center heigth_d" >J</div></div>
+</div>
+        <div class="row">
+
+<div class="col mt-3 d-block d-sm-none"><div class="p-3 border bg-primary text-center heigth_d" >J</div></div>
+<div class="col mt-3 d-none d-md-block d-lg-none d-none d-lg-block d-xl-none"><div class="p-3 border bg-primary text-center heigth_d" >K</div></div>
+  
+</div>
+
+        </div>
+        
+      
+    <script src="{{asset('/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
