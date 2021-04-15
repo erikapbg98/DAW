5aaa97d78bd385fc52eb3a9e3002ea885aa6a5f2
erikapbg98 <73491156+erikapbg98@users.noreply.github.com>
2021-04-14 23:33:09 -0600

    Plantillas

---
+++ Artesanias/resources/views/admin/users.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layouts.main')
+
+@section('contenido')
+<h2>Estoy en los users</h2>
+@endsection
+@section('scripts')
+<script>console.log("asdf");</script>
+@endsection
--- Artesanias/routes/web.php
+++ Artesanias/routes/web.php
@@ -2,24 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/', function () {
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+Route::get('/admin/usuarios', function () {
+    return view('admin.users');
+});
+Route::get('/admin/productos', function () {
+    return view('admin.productos');
+});
+Route::get('/practica', function () {
     return view('practica');
 });
-
 Route::get('/producto/{id}', function ($id) {
     return view('verproducto')->with('id',$id);
 });
